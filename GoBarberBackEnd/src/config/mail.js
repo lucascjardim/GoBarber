26195f49968ad5e4fec28eb5880f16c865621aa0
Lucas Costa jardim <lucascostajardim@iMac-de-Lucas.local>
2020-04-21 12:36:36 -0300

    Back-end e front-end web Gobarber Bootcamp Rkteat

---
+++ GoBarberBackEnd/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '522759ce44b13f',
+    pass: 'dfdc39e6a6385f'
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>'
+  }
+};
---
+++ GoBarberBackEnd/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://40da64cd66f34951bbbfa087c2266d64@sentry.io/1853247'
+};
